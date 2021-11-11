424e4e15f8e9ead34081c81e30e64fb111949c06
Matt Bianco <matthewbianco@protonmail.com>
2021-11-11 11:02:30 -0500

    made recipe schema

--- server/models/database.js
+++ server/models/database.js
@@ -6,3 +6,8 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function(){
     console.log('Connected')
 });
+
+// Models
+
+require('./Category')
+require('./Recipe')
